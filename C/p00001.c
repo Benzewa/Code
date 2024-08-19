41e5af55d55ee211589b6a02fd4935ce210c86dc
moutaz sami <moutazsami@moutazs-MacBook-Air.local>
2024-08-20 03:22:52 +0400

    Updates

--- C/p00001.c
+++ C/p00001.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 int main(void)
 {
-	printf("Hello World\n");
-	write(1,"HELLO WORLD\n",12);
+	printf("Hello World I am Mo3taz Sami\n");
+	write(1,"HELLO WORLD\n this is the write function in use",12);
 	return (0);
 }
