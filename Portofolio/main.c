27b28a1b1d93a71a9d83b374f38eb8b206018bfa
moutaz sami <moutazsami@moutazs-MacBook-Air.local>
2024-09-07 10:08:37 +0400

    UPDATE

--- Portofolio/main.c
+++ Portofolio/main.c
@@ -60,28 +60,28 @@ int main(void)
     {
         printf("odd\n");
     } */
-   
-   //control structures
-   /* int Grade = 0;
-   printf("Grade : ");
-   scanf("%d",&Grade);
 
-   if(Grade >= 90) printf("Passed GPA : A\n");
-   else if(Grade >= 80) printf("Passed GPA : B\n");
-   else if(Grade >= 70) printf("Passed GPA : C\n");
-   else if(Grade >= 60) printf("Passed GPA : D\n");
-   else if(Grade >= 50) printf("Passed GPA : E\n");
-   else printf("Failed\n"); */
-   
-   // looping
-   //while loop
-   
-   /* int i = 0;
-   while (i < 5)
-   {
-    printf("i : %d\n",i);
-    i+=1;
-   } */
+    // control structures
+    /* int Grade = 0;
+    printf("Grade : ");
+    scanf("%d",&Grade);
+
+    if(Grade >= 90) printf("Passed GPA : A\n");
+    else if(Grade >= 80) printf("Passed GPA : B\n");
+    else if(Grade >= 70) printf("Passed GPA : C\n");
+    else if(Grade >= 60) printf("Passed GPA : D\n");
+    else if(Grade >= 50) printf("Passed GPA : E\n");
+    else printf("Failed\n"); */
+
+    // looping
+    // while loop
+
+    /* int i = 0;
+    while (i < 5)
+    {
+     printf("i : %d\n",i);
+     i+=1;
+    } */
 
     /* int i = 0;
     int Sum = 0;
@@ -104,15 +104,46 @@ int main(void)
     }
     printf("The Sum is %d\n",Sum); */
 
-
     // infinte loop until user enter -1
-    int i = 0;
-    while(i != -1)
+    // and prints out the maximum number
+    /* int i = 0;
+    int max = 0;
+    while (i != -1)
     {
         printf("Enter A Number : ");
-        scanf("%d",&i);
+        scanf("%d", &i);
+        if (i > max)
+            max = i;
+        printf("Max : %d\n",max);
+    }
+    printf("Max number is %d\n", max); */
+
+    // arrays
+    int Grade[5] = {};
+    Grade[0] = 92;
+    Grade[1] = 97;
+    Grade[2] = 72;
+    Grade[3] = 82;
+    Grade[4] = 99;
+
+    int i = 0;
+    while (i < 5)
+    {
+        printf("%d\n", Grade[i]);
+        i += 1;
     }
 
+    i = 0;
+    int Counter = 0;
+    int TotalGrades = 0;
+    while (i < 5)
+    {
+        TotalGrades += Grade[i];
+        i++;
+        Counter++;
+    }
+    printf("The Total Grades is %d\n", TotalGrades);
+    printf("The Average of Grades is %d\n", (TotalGrades / Counter));
 
     return (0);
 }
