d40c56ab43ff36b049651810616fe1ee1682e29d
moutaz sami <moutazsami@moutazs-MacBook-Air.local>
2024-09-10 23:11:33 +0400

    UPDATE

---
+++ Portofolio/main7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// stopped Video on 3:35
+void addOne(int Arr[], int ArrLen);
+
+int main(void)
+{
+
+}
+void addOne(int Arr[], int ArrLen)
+{
+	int i = 0;
+	while (i < ArrLen)
+	{
+		Arr[i] += 1;
+		i++;
+	}
+}
