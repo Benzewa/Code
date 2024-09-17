2151ef17d06af8027536be56c0ccb8bc1b03e268
moutaz sami <moutazsami@moutazs-MacBook-Air.local>
2024-09-17 19:48:47 +0400

    UPDATE

---
+++ BroCode/brocodee05.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//stopped at 01:45
+int FindMax(int x, int y); // function prototype
+
+int main(void)
+{
+	int Num1 = 0, Num2 = 0;
+	printf("Enter Num1\n");
+	scanf("%d", &Num1);
+	printf("Enter Num2\n");
+	scanf("%d", &Num2);
+	printf("%d\n", FindMax(Num1, Num2));
+
+	return (0);
+}
+int FindMax(int x, int y)
+{
+	return (x > y) ? x : y;
+}
