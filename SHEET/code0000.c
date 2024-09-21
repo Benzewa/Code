8246f9c93acf0986e32175312adc1e680a6b194c
moutaz sami <moutazsami@moutazs-MacBook-Air.local>
2024-09-21 14:25:04 +0400

    UPDATE

---
+++ SHEET/code0000.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    char Name[100];
+    scanf("%s", Name);
+    printf("Hello, %s", Name);
+    return (0);
+}
---
+++ SHEET/code0000.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    string S1 = "";
+    cin >> S1;
+    cout<<"Hello, "<< S1;
+}
