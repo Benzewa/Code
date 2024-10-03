5e3b1cfaa9ba94e82ca37b47e40d124ecc809f87
motaz <motaz@motaz.local>
2024-10-04 00:28:44 +0400

    UPDATE

---
+++ Piscine/C02/ft_str_is_alpha.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int ft_str_is_alpha(char *str);
+int main(void)
+{
+	return 0;
+}
+int ft_str_is_alpha(char *str)
+{
+	int i = 0;
+	while(str[i] != '\0')
+	{
+		if((str[i] < 'a' || str[i] > 'z') && 
+	}
+}
--- Piscine/C02/ft_strncpy.c
+++ Piscine/C02/ft_strncpy.c
@@ -1,7 +1,24 @@
 #include <unistd.h>
 #include <stdio.h>
-char *str(char *dest, char *src,  
+#include <string.h>
+char *ft_strncpy(char *dest, char *src, int n);  
 int main(void)
 {
-
+	char dest1[20];
+	char dest2[20];
+	ft_strncpy(dest1, "moutaz sami shaban", 3);
+	printf("%s\n", dest1);
+	strncpy(dest2, "moutaz sami shaban", 3);
+	printf("%s\n", dest2);
+}
+char *ft_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return(dest);
 }
