f3d6662ff2e62c199087cafd20939614e9d4de42
motaz <motaz@motaz.local>
2024-10-12 17:25:39 +0400

    UPDATE

--- HadHod/C4/p015.cpp
+++ HadHod/C4/p015.cpp
@@ -5,12 +5,17 @@ using namespace std;
 void ReadNums(int &Num1, int &Num2, int &Num3);
 int Sum(int Num1, int Num2, int Num3);
 void PrintRes(int Sum);
+double Avg(int Num1, int Num2, int Num3);
 
 int main(void)
 {
     int Num1, Num2, Num3;
     ReadNums(Num1, Num2, Num3);
+    cout<<"sum : ";
     PrintRes(Sum(Num1, Num2, Num3));
+    cout<<"\nAvg : ";
+    PrintRes(Avg(Num1, Num2, Num3));
+    return 0;
 }
 void ReadNums(int &Num1, int &Num2, int &Num3)
 {
@@ -24,6 +29,10 @@ void ReadNums(int &Num1, int &Num2, int &Num3)
 int Sum(int Num1, int Num2, int Num3)
 {
     return Num1 + Num2 + Num3;
+}
+double Avg(int Num1, int Num2, int Num3)
+{
+    return Sum(Num1, Num2, Num3) / 3.0;
 }
 void PrintRes(int Sum)
 {
