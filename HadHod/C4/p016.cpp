7e8b90da2c284c7497369606538245443b214ec7
motaz <motaz@motaz.local>
2024-10-13 23:36:04 +0400

    UPDATE

--- HadHod/C4/p016.cpp
+++ HadHod/C4/p016.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
+
 enum enPass
 {
 	Fail,
 	Pass
 };
+
 int main(void)
 {
 	// comments are really helpful bro
 	int Num1, Num2, Num3;
 	cout << "hello ";
 	cout << "My name is moutaz\n";
-	
+
 	return 0;
 }
