223d3098dabf9154b71880f32d3d06834c607929
motaz <motaz@motaz.local>
2024-10-19 00:34:33 +0400

    UPDATE

--- HadHod/C4/p023.cpp
+++ HadHod/C4/p023.cpp
@@ -1,27 +1,29 @@
 #include<iostream>
-//pow 
-//sqrt
+using namespace std;
+
+// -pow-
+// -sqrt-
 // pi * r squared
 
-double ReadRadius(double &Radius);
+void ReadRadius(double &Radius);
 double CalcArea(double Radius);
 void PrintArea(double Num);
 
 int main(void)
 {
     double Radius = 0;
-
-    PrintArea(CalcArea(ReadRadius(Radius)));
+    ReadRadius(Radius);
+    PrintArea(CalcArea(Radius));
     return 0;
 }
-double ReadRadius(double &Radius)
+void ReadRadius(double &Radius)
 {
     cout << "Enter Radius\n";
     cin >> Radius;
-    return Radius;
 }
 double CalcArea(double Radius)
 {
+    // -const cannot be changed-
     const double PI = 3.141592653589793238;
     return PI * pow(Radius, 2);
 }
