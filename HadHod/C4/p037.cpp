76120b989ad3955aadce4f6980e365b0d43d0bcd
motaz <motaz@motaz.local>
2024-10-22 16:44:44 +0400

    UPDATE

--- HadHod/C4/p037.cpp
+++ HadHod/C4/p037.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+void ReadNum(int &Num, string Message);
+int CalcPower(int Num, int Power);
+void Print(int Num);
 int main()
 {
     int Num = 0;
-    cin >> Num;
+    ReadNum(Num, "Please Enter   Num");
     int Pow = 0;
-    cin >> Pow;
-    int Times = 1;
-    for (int i = 0 ; i < Pow ; i++)
-    {
-        Times = Num * Times;
-    }
-    cout << Times;
+
+    ReadNum(Pow, "Please Enter Power");
+    Print(CalcPower(Num, Pow));
+
+    ReadNum(Pow, "Please Enter Power");
+    Print(CalcPower(Num, Pow));
+
+    ReadNum(Pow, "Please Enter Power");
+    Print(CalcPower(Num, Pow));
 }
-int ReadNum(string Message)
+void ReadNum(int &Num, string Message)
 {
+    cout << Message << " : ";
+    cin >> Num;
+}
+int CalcPower(int Num, int Power)
+{
+    if (Power == 0)
+        return 1;
+
+    int Times = 1;
+    for (int i = 0 ; i < Power ; i++)
+    {
+        Times = Times * Num;
+    }
+    return Times;
+}
+void Print(int Num)
+{
+    cout << "The  Solution   is : " << Num << "\n";
 }
