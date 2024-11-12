bf1ff7ee97993fb54ee0c3b4eb7e9ff0e31c699a
motaz <motaz@motaz.local>
2024-11-12 22:36:26 +0400

    UPDATE

--- HadHod/C5/p032.cpp
+++ HadHod/C5/p032.cpp
@@ -5,25 +5,26 @@
 #include <string>
 using namespace std;
 
-//string ReadPass(string Msg);
-void Print();
+string ReadPass(string Msg);
+void Print(string Pass);
 
 int main()
 {
-    Print();
+    Print(ReadPass("Please Enter Pass : "));
 }
-//string ReadPass(string Msg)
-//{
-//    string Input = "";
-//    while(Input == "")
-//    {
-//        cout << Msg;
-//        getline(cin, Input);
-//    }
-//    return Input;
-//}
-void Print()
+string ReadPass(string Msg)
 {
+    string Input = "";
+    while(Input == "")
+    {
+        cout << Msg;
+        getline(cin, Input);
+    }
+    return Input;
+}
+void Print(string Pass)
+{
+    bool Flag = false;
     string Word = "";
     int Counter = 0;
     for(int i = 'A' ; i <= 'C' ; i++)
@@ -32,18 +33,26 @@ void Print()
         {
             for(int k = 'A'; k <= 'C';k++)
             {
-                //Counter++;
+                Counter++;
                 Word = Word + char(i);
                 Word = Word + char(j);
                 Word = Word + char(k);
-                //cout << "Trial " << Counter << " : " << Word << "\n";
+                cout << "Trial " << Counter << " ";
                 cout << Word << "\n";
-                //if(Word == Pass)
-                //{
-                //    cout << "Found At Trial .";
-                //}
+                if(Word == Pass)
+                    Flag = true;
                 Word = "";
+                if(Flag)
+                {
+                    cout << "Found at : " << Counter;
+                    break;
+                }
             }
+            if(Flag)
+                break;
+            cout << "\n";
         }
+        if(Flag)
+            break;
     }
 }
