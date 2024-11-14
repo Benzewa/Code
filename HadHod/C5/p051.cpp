08e02a78a0d8c9ec6489e31b6db2425449e74278
motaz <motaz@motaz.local>
2024-11-14 18:28:56 +0400

    UPDATE

---
+++ HadHod/C5/p051.cpp
@@ -0,0 +1,15 @@
+// vid 22 C5
+
+// using &num will make you change the value of num and affect the value of main 
+// call by ref
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+
+}
