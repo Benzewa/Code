7aa7b61edfab9046b0e3f493347c270c84d2ab06
motaz <motaz@motaz.local>
2024-11-14 14:49:53 +0400

    UPDATE

---
+++ HadHod/C5/p047.cpp
@@ -0,0 +1,12 @@
+// print random small letter , capital letter, symbol and Digit
+// vid 20 C5
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World\n";
+}
