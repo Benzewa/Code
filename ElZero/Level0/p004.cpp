f88692eac33d227e2ed13cb62779b039caa18e92
motaz <motaz@motaz.local>
2024-11-20 21:01:51 +0400

    UPDATE

---
+++ ElZero/Level0/p004.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+}
+string Reverse(string Word)
+{
+    int StrLen = 0;
+    for(int i = 0;i < Word.length();i++)
+    {
+
+    }
+}
+int StrLen(string Word)
+{
+    for(;Word;)
+    {
+
+    }
+}
---
+++ HadHod/C5/p089.cpp
@@ -0,0 +1,11 @@
+// vid 40 C5
+// Copy Numbers without repeatetion
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    cout << "Hello World\n";
+}
