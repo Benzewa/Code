d4f7179034f4bfe0935f2f6caa50573bfa9fef8c
motaz <motaz@motaz.local>
2024-12-08 09:53:00 +0400

    UPDATE

--- HadHod/C6/p005.cpp
+++ HadHod/C6/p005.cpp
@@ -11,8 +11,14 @@ using namespace std;
 int main()
 {
     srand((unsigned)time(NULL));
+
     int ArrSrc[] = {1,2,3,4,5};
     for(int i : ArrSrc)
+        cout << i << " ";
+    cout << "\n";
+
+    // {1,2,3,4,5,6,7,8,9} Set
+    for(int i : {1,2,3,4,5,6,7,8,9})
         cout << i << " ";
     cout << "\n";
 
