1f721e6b541c7927d5f84a13fa1881aa3d84624e
MzSamyCpp <motaz@motaz.local>
2025-02-19 00:23:02 +0400

    Update

--- HadHod/RC7/p005.cpp
+++ HadHod/RC7/p005.cpp
@@ -5,13 +5,21 @@
 using namespace std;
 
 int Random(int From, int To);
-void FillArr(int Arr[3][3], int Rows, int Cols);
-void PrintArr(int Arr[3][3], int Rows, int Cols);
+void FillArrSrc(int Arr[3][3], int Rows, int Cols);
+void PrintArrSrc(int Arr[3][3], int Rows, int Cols);
+void FillArrDes(int ArrSrc[3][3], int Rows, int Cols, int ArrDes[3]);
+int ColSum(int Arr[3][3], int Rows, int ColNum);
+void PrintArrDes(int Arr[3], int Rows, int Cols);
 
 int main()
 {
     srand((unsigned)time(NULL));
-    cout << "Hello World\n";
+    int ArrSrc[3][3];
+    int ArrDes[3];
+    FillArrSrc(ArrSrc, 3, 3);
+    PrintArrSrc(ArrSrc, 3, 3);
+    FillArrDes(ArrSrc, 3, 3, ArrDes);
+    PrintArrDes(ArrDes, 3, 3);
 }
 int Random(int From, int To)
 {
@@ -27,7 +35,7 @@ void FillArrSrc(int Arr[3][3], int Rows, int Cols)
         }
     }
 }
-void PrintArr(int Arr[3][3], int Rows, int Cols)
+void PrintArrSrc(int Arr[3][3], int Rows, int Cols)
 {
     for (int i = 0; i < Rows; i++)
     {
@@ -42,6 +50,7 @@ void FillArrDes(int ArrSrc[3][3], int Rows, int Cols, int ArrDes[3])
 {
     for (int j = 0; j < Cols; j++)
     {
+        ArrDes[j] = ColSum(ArrSrc, Rows, j);
     }
 }
 int ColSum(int Arr[3][3], int Rows, int ColNum)
@@ -52,4 +61,11 @@ int ColSum(int Arr[3][3], int Rows, int ColNum)
         Sum += Arr[i][ColNum];
     }
     return (Sum);
+}
+void PrintArrDes(int Arr[3], int Rows, int Cols)
+{
+    for (int j = 0; j < Cols; j++)
+    {
+        cout << setw(3) << Arr[j] << "\n";
+    }
 }
