1679d95a24e7df903fe37a2b6f60bdf5e737c1b5
MzSamyCpp <motaz@motaz.local>
2025-02-21 19:29:32 +0400

    Update

--- HadHod/RC7/p013.cpp
+++ HadHod/RC7/p013.cpp
@@ -4,13 +4,58 @@
 #include <iomanip>
 
 int Random(int From, int To);
+void FillArr(int Arr[3][3], int Rows, int Cols);
+void PrintArr(int Arr[3][3], int Rows, int Cols);
+int SumArr(int Arr[3][3], int Rows, int Cols);
 
 int main()
 {
     srand((unsigned)time(NULL));
-    std::cout << Random(0, 5);
+    int ArrOne[3][3] = {0};
+
+    FillArr(ArrOne, 3, 3);
+    std::cout << "Arr : ";
+    std::cout << "\n";
+    PrintArr(ArrOne, 3, 3);
+    std::cout << "\n";
+    std::cout << "Sum : " << SumArr(ArrOne, 3, 3);
+    std::cout << "\n";
 }
+
 int Random(int From, int To)
 {
     return (rand() % (To - From + 1) + From);
+}
+void FillArr(int Arr[3][3], int Rows, int Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            Arr[i][j] = Random(0, 5);
+        }
+    }
+}
+void PrintArr(int Arr[3][3], int Rows, int Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            std::cout << Arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+int SumArr(int Arr[3][3], int Rows, int Cols)
+{
+    int Sum = 0;
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            Sum += Arr[i][j];
+        }
+    }
+    return (Sum);
 }
