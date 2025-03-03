cea977c429bf5052e1160d4e46bf80bbec4f3643
MzSamyCpp <motaz@motaz.local>
2025-03-04 00:34:31 +0400

    Update

---
+++ Backend/Month1/p0001.py
@@ -0,0 +1,19 @@
+# Python Basix
+
+# Variables
+StudentsCount = 1000 # integers
+Rating = 4.99 # float
+IsPublished = True # boolean
+CourseName = "Pyhton Programming" # string
+
+print(StudentsCount)
+print(Rating)
+print(IsPublished)
+print(CourseName)
+
+# Strings
+print(len(CourseName))
+print(CourseName[0])
+print(CourseName[5])
+print(CourseName[-1])
+print(CourseName[0:3])
