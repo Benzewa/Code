2b03e427abd1a7a806e1e3eb6251d07e9699a08f
MzSamyCpp <motaz@motaz.local>
2025-03-06 00:08:19 +0400

    Update

---
+++ Backend/Month1/p0006.py
@@ -0,0 +1,16 @@
+# type conversions
+
+# input takes the input as string
+
+x = input("x : ")
+y = int(x) + 5
+
+print(type(x))
+print(type(y))
+print(y)
+
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
+
---
+++ Backend/Month1/p0007.py
@@ -0,0 +1,17 @@
+x = 5
+y = 6
+
+print(f"x : { x }, y : { y }")
+
+
+
+# Falsy and Truly
+
+# Falsy
+# ""
+# 0
+# None
+
+# Any thing else is truly
+
+# we stopped the video at 1 hour
