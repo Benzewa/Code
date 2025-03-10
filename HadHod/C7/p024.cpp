b822d2b618daf33945505f4e702434802b171510
MzSamyCpp <motaz@motaz.local>
2025-03-10 23:07:35 +0400

    Update

---
+++ HadHod/C7/p024.cpp
@@ -0,0 +1,26 @@
+// vid 15
+// program that Counts a Number in Matrix
+
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
+
+void FillArr(int Arr[3][3], int Rows, int Cols);
+void PrintArr(int Arr[3][3], int Rows, int Cols);
+int CountNum(int Arr[3][3], int Rows, int Cols, int Number);
+
+int main()
+{
+    srand((unsigned)time(NULL));
+}
+void FillArr(int Arr[3][3], int Rows, int Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            
+        }
+    }
+}
