13eccedf804ce5ab4ffe5c94c352f7e4da7adc9d
MzSamyCpp <motaz@motaz.local>
2025-03-12 01:18:02 +0400

    Update

--- HadHod/C7/p028.cpp
+++ HadHod/C7/p028.cpp
@@ -9,6 +9,8 @@
 int Random(int From, int To);
 void FillArr(int Arr[3][3], int Rows, int Cols);
 void PrintArr(int Arr[3][3], int Rows, int Cols);
+int MaxNum(int Arr[3][3], int Rows, int Cols);
+int MinNum(int Arr[3][3], int Rows, int Cols);
 
 int main()
 {
@@ -17,11 +19,59 @@ int main()
     int Arr[3][3] = {0};
     FillArr(Arr, 3, 3);
     PrintArr(Arr, 3, 3);
+    printf("\n");
+    printf("Max Number : %d\n", MaxNum(Arr, 3, 3));
+    printf("Min Number : %d\n", MinNum(Arr, 3, 3));
 }
 
 int Random(int From, int To)
 {
-    return
+    return (rand() % (To - From + 1) + From);
 }
-void FillArr(int Arr[3][3], int Rows, int Cols);
-void PrintArr(int Arr[3][3], int Rows, int Cols);
+void FillArr(int Arr[3][3], int Rows, int Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            Arr[i][j] = Random(10, 99);
+        }
+    }
+}
+void PrintArr(int Arr[3][3], int Rows, int Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            std::cout << std::setw(3) << Arr[i][j] << " ";
+        }
+        printf("\n");
+    }
+}
+int MaxNum(int Arr[3][3], int Rows, int Cols)
+{
+    int Num = Arr[0][0];
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            if (Arr[i][j] > Num)
+                Num = Arr[i][j];
+        }
+    }
+    return (Num);
+}
+int MinNum(int Arr[3][3], int Rows, int Cols)
+{
+    int Num = Arr[0][0];
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            if (Arr[i][j] < Num)
+                Num = Arr[i][j];
+        }
+    }
+    return (Num);
+}
