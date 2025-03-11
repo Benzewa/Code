22f15f46e21068006f6b1e87f9b36b0c6cdc1be0
MzSamyCpp <motaz@motaz.local>
2025-03-11 14:00:45 +0400

    Update

---
+++ HadHod/C7/p028.cpp
@@ -0,0 +1,27 @@
+// vid 19
+// program that Prints Min/Max Numbers in the Matrix
+
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
+
+int Random(int From, int To);
+void FillArr(int Arr[3][3], int Rows, int Cols);
+void PrintArr(int Arr[3][3], int Rows, int Cols);
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int Arr[3][3] = {0};
+    FillArr(Arr, 3, 3);
+    PrintArr(Arr, 3, 3);
+}
+
+int Random(int From, int To)
+{
+    return 
+}
+void FillArr(int Arr[3][3], int Rows, int Cols);
+void PrintArr(int Arr[3][3], int Rows, int Cols);
